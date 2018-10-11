fffae43723c4a75ef86568e2d581cf7f3d784657
Andres Contreras <andres_c8@outlook.com>
2018-10-11 10:47:49 -0500

    se crea pantalla ubicacion

---
+++ app/src/main/java/innovatech/smartservices/fragments/PubUbicacionFragment.java
@@ -0,0 +1,15 @@
+package innovatech.smartservices.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import innovatech.smartservices.R;
+
+public class PubUbicacionFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_pub_ubicacion);
+    }
+}
