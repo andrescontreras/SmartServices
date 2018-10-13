5b5c971ed47e5b46ffac215d14503c3dad282eb6
MiguelAPaez <miguelangelpaezramos@gmail.com>
2018-10-13 18:01:22 -0500

    Creación de pantallas de Detalles y Servicios Destacados

---
+++ app/src/main/java/innovatech/smartservices/fragments/ServiciosDestacadosFragment.java
@@ -0,0 +1,19 @@
+package innovatech.smartservices.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import innovatech.smartservices.R;
+
+public class ServiciosDestacadosFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_destacados_servicio, container, false);
+        return view;
+    }
+}
