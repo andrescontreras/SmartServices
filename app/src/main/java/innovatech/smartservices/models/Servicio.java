38014fbd9b4c2732aaf8b04f6d68a64b4e52e083
gitAccCarlo <cardavid1997@gmail.com>
2018-10-18 09:14:23 -0500

    cambios en clases servicio, creacion de clase disponibilidad y activtyNuevaPublicaicon

--- app/src/main/java/innovatech/smartservices/models/Servicio.java
+++ app/src/main/java/innovatech/smartservices/models/Servicio.java
@@ -11,17 +11,29 @@ public class Servicio {
     int precio;
     int promedioCalificacion;
     List<File> fotos= new ArrayList<File>();
+    int fechaActivacion;
+    Boolean posicionamiento;
+    String tipo;
+    List<Ubicacion> ubicacion;
+    List<Disponiblidad>calendario;
 
     public Servicio(){
 
     }
 
-    public Servicio(String descripcion, String nombre, int precio, int promedioCalificacion) {
+    public Servicio(String descripcion, String nombre, int precio, int promedioCalificacion, List<File> fotos, int fechaActivacion, Boolean posicionamiento, String tipo, List<Ubicacion> ubicacion, List<Disponiblidad> calendario) {
         this.descripcion = descripcion;
         this.nombre = nombre;
         this.precio = precio;
         this.promedioCalificacion = promedioCalificacion;
+        this.fotos = fotos;
+        this.fechaActivacion = fechaActivacion;
+        this.posicionamiento = posicionamiento;
+        this.tipo = tipo;
+        this.ubicacion = ubicacion;
+        this.calendario = calendario;
     }
+
 
     public String getDescripcion() {
         return descripcion;
@@ -55,4 +67,52 @@ public class Servicio {
         this.promedioCalificacion = promedioCalificacion;
     }
 
+    public List<File> getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(List<File> fotos) {
+        this.fotos = fotos;
+    }
+
+    public int getFechaActivacion() {
+        return fechaActivacion;
+    }
+
+    public void setFechaActivacion(int fechaActivacion) {
+        this.fechaActivacion = fechaActivacion;
+    }
+
+    public Boolean getPosicionamiento() {
+        return posicionamiento;
+    }
+
+    public void setPosicionamiento(Boolean posicionamiento) {
+        this.posicionamiento = posicionamiento;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Ubicacion> getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(List<Ubicacion> ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public List<Disponiblidad> getCalendario() {
+        return calendario;
+    }
+
+    public void setCalendario(List<Disponiblidad> calendario) {
+        this.calendario = calendario;
+    }
 }
+
