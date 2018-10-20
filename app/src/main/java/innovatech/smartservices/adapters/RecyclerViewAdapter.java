4c9a116d978151cfdbd2b3de30036dcaa23bda2d
ssalamanca <santiago.anime@hotmail.com>
2018-10-20 18:42:05 -0500

    Se muestra el grid view en destacados, falta arreglar imagenes que vengan de Firebase

--- app/src/main/java/innovatech/smartservices/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/innovatech/smartservices/adapters/RecyclerViewAdapter.java
@@ -1,10 +1,14 @@
 package innovatech.smartservices.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -14,30 +18,25 @@ import innovatech.smartservices.models.Servicio;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
-    private ServiciosDestacadosFragment mContext;
+    private Context mContext;
     private List<Servicio> mData;
 
-    public RecyclerViewAdapter(ServiciosDestacadosFragment nContext , List <Servicio> mData) {
-        this.mContext = nContext;
+    public RecyclerViewAdapter(Context mContext , List <Servicio> mData) {
+        this.mContext = mContext;
         this.mData = mData;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent , int viewType) {
-    /*
-        View view;
-        LayoutInflater mInflater = LayoutInflater.from ( mContext );
-        view = mInflater.inflate ( R.layout.cardview_item_servicio,parent,false );
+        View view = LayoutInflater.from(mContext).inflate(R.layout.cardview_item_servicio,parent,false);
         return new MyViewHolder ( view );
-        */
-    return null;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder , int position) {
 
         holder.tv_servicio_title.setText(mData.get(position).getNombre());
-        //holder.img_servicio_thumbnail.setImageResource ( mData.get(position).getThumbnail () );
+        Picasso.with(mContext).load(mData.get(position).getFotos().get(0)).into(holder.img_servicio);
 
     }
 
@@ -49,13 +48,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
         TextView tv_servicio_title;
-        ImageView img_servicio_thumbnail;
+        ImageView img_servicio;
 
         public MyViewHolder(View itemView) {
             super ( itemView );
-
             tv_servicio_title = (TextView) itemView.findViewById ( R.id.servicio_title_id );
-            img_servicio_thumbnail = (ImageView) itemView.findViewById ( R.id.servicio_img_id );
+            img_servicio = (ImageView) itemView.findViewById ( R.id.servicio_img_id );
 
         }
     }
--- app/src/main/java/innovatech/smartservices/fragments/ServiciosDestacadosFragment.java
+++ app/src/main/java/innovatech/smartservices/fragments/ServiciosDestacadosFragment.java
@@ -1,5 +1,6 @@
 package innovatech.smartservices.fragments;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,30 +12,68 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import innovatech.smartservices.R;
+import innovatech.smartservices.adapters.RecyclerViewAdapter;
 import innovatech.smartservices.models.Servicio;
 
 import static android.support.v7.widget.LinearLayoutManager.*;
 
 public class ServiciosDestacadosFragment extends Fragment {
-    List<Servicio> lstServicio;
+    List<Servicio> lstServicio =  new ArrayList<Servicio>();
+    RecyclerView myrv;
+    FirebaseAuth mAuth ;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         final View view = inflater.inflate(R.layout.fragment_destacados_servicio, container, false);
-
-        lstServicio = new ArrayList <> ();
-        //lstServicio.add(new Servicio(//Nombre Servicio, //Categoria Servicio, //Descripcion Servicio, //Imagen Servicio));
-        //RecyclerView myrv = (RecyclerView) findViewById (R.id.recyclerview_id) ;
-        //RecyclerViewAdapter myAdapter = new RecyclerViewAdapter (this,lstServicio);
-        //myrv.setLayoutManager ( new GridLayoutManager ( this,3 ) );
-        //myrv.setAdapter(myAdapter);
+        mAuth = FirebaseAuth.getInstance();
+        myrv = (RecyclerView) view.findViewById(R.id.recyclerview_id);
+        myrv.setHasFixedSize(true);
+        myrv.setLayoutManager ( new GridLayoutManager ( getActivity(),2 ) );
+        agregarServicios();
         return view;
     }
+    public void agregarServicios(){
+        lstServicio = new ArrayList <> ();
 
+        FirebaseUser user = mAuth.getCurrentUser();
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("servicios");
+        db.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if(snapshot!=null){
+                        Servicio serv = snapshot.getValue(Servicio.class);
+                        lstServicio.add(serv);
+                    }
+                    else{
+                        Toast.makeText(getActivity(), "Hubo un problema encontrando los servicios", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                //lstServicio.add(new Servicio(//Nombre Servicio, //Categoria Servicio, //Descripcion Servicio, //Imagen Servicio));
+                RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(getActivity(),lstServicio);
+                myrv.setHasFixedSize(true);
+                myrv.setLayoutManager ( new GridLayoutManager ( getActivity(),2 ) );
+                myrv.setAdapter(myAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                throw databaseError.toException();
+            }
+        });
+    }
 }
