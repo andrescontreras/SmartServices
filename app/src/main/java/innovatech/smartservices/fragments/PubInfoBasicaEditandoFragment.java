b0dd880585e9ef308ddcd2fa123d58733567dad1
gitAccCarlo <cardavid1997@gmail.com>
2018-11-06 19:01:05 -0500

    mas cambios de precarga de info

--- app/src/main/java/innovatech/smartservices/fragments/PubInfoBasicaEditandoFragment.java
+++ app/src/main/java/innovatech/smartservices/fragments/PubInfoBasicaEditandoFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -33,7 +34,9 @@ import java.util.List;
 
 import innovatech.smartservices.R;
 import innovatech.smartservices.adapters.ImageAdapter;
+import innovatech.smartservices.adapters.RecyclerViewEditandoImagenesAdapter;
 import innovatech.smartservices.models.Servicio;
+import innovatech.smartservices.models.Ubicacion;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -48,9 +51,10 @@ public class PubInfoBasicaEditandoFragment extends Fragment {
     private static final int RESULT_LOAD_IMAGE = 1;
     private ProgressDialog nProgressDialog;
     private ImageAdapter mAdapter;
-    private RecyclerView mRecyclerView;
+    private RecyclerView myrv;
     private List<Uri> listaImagenes = new ArrayList<Uri>();
     List<Servicio> lstServicio =  new ArrayList<Servicio>();
+    RecyclerViewEditandoImagenesAdapter myAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -63,9 +67,9 @@ public class PubInfoBasicaEditandoFragment extends Fragment {
         precio = (EditText)view.findViewById(R.id.txtPrecio);
         selecImagen = (Button)view.findViewById(R.id.btn_agregarImagenes);
         sig = (Button)view.findViewById(R.id.btn_sig_detalles);
-        mRecyclerView= view.findViewById(R.id.recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myrv = view.findViewById(R.id.recycler_view);
+        myrv.setHasFixedSize(true);
+        myrv.setLayoutManager(new LinearLayoutManager(getActivity()));
         cargarInfoAnterior(savedInstanceState,view,mAuth);
         accionBotones(view);
         return view;
@@ -86,13 +90,13 @@ public class PubInfoBasicaEditandoFragment extends Fragment {
 
                 }
                 mAdapter = new ImageAdapter(getActivity(),listaImagenes);
-                mRecyclerView.setAdapter(mAdapter);
+                myrv.setAdapter(mAdapter);
                 Toast.makeText(getActivity(), "Se subieron los archivos", Toast.LENGTH_SHORT).show();
             }else if(data.getData()!=null){
                 Uri uri = data.getData();
                 listaImagenes.add(uri);
                 mAdapter = new ImageAdapter(getActivity(),listaImagenes);
-                mRecyclerView.setAdapter(mAdapter);
+                myrv.setAdapter(mAdapter);
 
             }
         }
@@ -163,12 +167,18 @@ public class PubInfoBasicaEditandoFragment extends Fragment {
                 }
                 if(seguir){
                     System.out.println("si entra a seguir");
+                    List<Servicio>aux=new ArrayList<Servicio>();
                     for(int i=0;i<lstServicio.size();i++){
                         if(lstServicio.get(i).getId()!=null && idServ!=null){
                             System.out.println("si entra al if de que no son nulos");
                             if(lstServicio.get(i).getId().equals(idServ)){
                                 nombre.setText(lstServicio.get(i).getNombre().toString());
                                 precio.setText(String.valueOf(lstServicio.get(i).getPrecio()));
+                                aux.add(lstServicio.get(i));
+                                myAdapter=new RecyclerViewEditandoImagenesAdapter(getActivity(),aux);
+                                myrv.setHasFixedSize(true);
+                                myrv.setLayoutManager(new LinearLayoutManager(getActivity()));
+                                myrv.setAdapter(myAdapter);
 
                             }
                         }
